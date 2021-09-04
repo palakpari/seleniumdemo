d8853f701006e4db69a599080f0faa3d67d4ac33
palakpari patel <palakparipatel@gmail.com>
2021-09-04 09:49:03 -0400

    formats code

--- src/test/java/org/example/HomePageTest.java
+++ src/test/java/org/example/HomePageTest.java
@@ -11,12 +11,10 @@ import static org.testng.Assert.assertEquals;
 public class HomePageTest {
 
     @Test
-    public void landOnGoogleHomePage(){
-
+    public void landOnGoogleHomePage() {
         System.setProperty("webdriver.chrome.driver",
                 System.getProperty("user.dir") + "/src/test/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://google.com");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         String title = driver.getTitle();
