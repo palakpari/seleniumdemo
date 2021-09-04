f6496ed0b605256133589a70024f6920c89a0f8c
palakpari patel <palakparipatel@gmail.com>
2021-09-04 09:41:10 -0400

    adds logic to verify google home page

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -4,17 +4,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+
 public class LoginTest {
 
     @Test
-    public void successfulLoginTest(){
+    public void landOnGoogleHomePage(){
 
         System.setProperty("webdriver.chrome.driver",
                 System.getProperty("user.dir") + "/src/test/resources/chromedriver");
-
         WebDriver driver = new ChromeDriver();
+
         driver.get("https://google.com");
-
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String title = driver.getTitle();
+        driver.quit();
+        assertEquals(title, "Yahoo");
     }
 }
